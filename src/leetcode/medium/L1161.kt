bfbbe95268728d1f1779bf6965e9e63d3ce37e42
arsh <arshfodcc@gmail.com>
2021-04-18 12:57:52 +0300

    Try a different approach with more edge cases

--- src/leetcode/medium/L1161.kt
+++ src/leetcode/medium/L1161.kt
@@ -1,11 +1,12 @@
 package leetcode.medium
 
+
 fun main() {
-    val root = TreeNode(1)
-    root.left = TreeNode(7)
-    root.right = TreeNode(0)
-    root.left?.left = TreeNode(7)
-    root.left?.right = TreeNode(-8)
+//    val root = TreeNode(1)
+//    root.left = TreeNode(7)
+//    root.right = TreeNode(0)
+//    root.left?.left = TreeNode(7)
+//    root.left?.right = TreeNode(-8)
 
 //    val root = TreeNode(989)
 //    root.right = TreeNode(10250)
@@ -13,38 +14,75 @@ fun main() {
 //    root.right?.right = TreeNode(-89388)
 //    root.right?.right?.right = TreeNode(-32127)
 
+//    val root = TreeNode(-100)
+//    root.left = TreeNode(-200)
+//    root.right = TreeNode(-300)
+//    root.left?.left = TreeNode(-20)
+//    root.left?.right = TreeNode(-5)
+//    root.right?.left = TreeNode(-10)
+
+    val root = TreeNode(1)
+    root.left = TreeNode(7)
+    root.right = TreeNode(0)
+    root.left?.left = TreeNode(7)
+    root.left?.right = TreeNode(-8)
+    root.left?.right?.right = TreeNode(10)
+
     println(maxLevelSum(root))
 }
 
 fun maxLevelSum(root: TreeNode?): Int {
-    // TODO: Incomplete
-    var max = root?.`val` ?: 0
+    var max = 0
+    var temp = root
     var lvl = 1
-    var left = root?.left
-    var right = root?.right
 
-    while (left != null || right != null) {
+    while (temp?.left != null || temp?.right != null) {
         var sum = 0
 
-        if (left?.`val` != null) {
-            sum += maxLevelSum(left)
+        if (root?.left?.`val` != null)
+            sum += levelMax(temp.left)
 
-            left = left.left
-        }
+        if (root?.right?.`val` != null)
+            sum += levelMax(temp.right)
 
-        if (right?.`val` != null) {
-            sum += maxLevelSum(right)
-
-            right = right.right
-        }
-
-        if (sum > max) {
+        if (sum > max || lvl == 1) {
             max = sum
             lvl++
         }
+
+        temp = temp.left
+    }
+
+    while (temp?.left != null || temp?.right != null) {
+        var sum = 0
+
+        if (root?.left?.`val` != null)
+            sum += levelMax(temp.left)
+
+        if (root?.right?.`val` != null)
+            sum += levelMax(temp.right)
+
+        if (sum > max || lvl == 1) {
+            max = sum
+            lvl++
+        }
+
+        temp = temp.right
     }
 
     return lvl
+}
+
+fun levelMax(root: TreeNode?): Int {
+    var max = 0
+
+    if (root?.left?.`val` != null)
+        max += root.left!!.`val`
+
+    if (root?.right?.`val` != null)
+        max += root.right!!.`val`
+
+    return max
 }
 
 class TreeNode(var `val`: Int) {
--- src/leetcode/medium/L1161Test.kt
+++ src/leetcode/medium/L1161Test.kt
@@ -19,6 +19,31 @@ class L1161Test {
 
     @Test
     fun maxLevelSumB() {
+        // [1,7,0,7,-8,null,null]
+        val root = TreeNode(1)
+        root.left = TreeNode(7)
+        root.right = TreeNode(0)
+        root.left?.left = TreeNode(7)
+        root.left?.right = TreeNode(-8)
+        root.left?.right?.right = TreeNode(10)
+
+        assertEquals(4, maxLevelSum(root))
+    }
+
+    @Test
+    fun maxLevelSumC() {
+        // [1,7,null,null,0,7,-8]
+        val root = TreeNode(1)
+        root.left = TreeNode(7)
+        root.right = TreeNode(0)
+        root.right?.left = TreeNode(7)
+        root.right?.right = TreeNode(-8)
+
+        assertEquals(2, maxLevelSum(root))
+    }
+
+    @Test
+    fun maxLevelSumD() {
         // [989,null,10250,98693,-89388,null,null,null,-32127]
         val root = TreeNode(989)
         root.right = TreeNode(10250)
@@ -27,5 +52,18 @@ class L1161Test {
         root.right?.right?.right = TreeNode(-32127)
 
         assertEquals(2, maxLevelSum(root))
+    }
+
+    @Test
+    fun maxLevelSumE() {
+        // [-100,-200,-300,-20,-5,-10,null]
+        val root = TreeNode(-100)
+        root.left = TreeNode(-200)
+        root.right = TreeNode(-300)
+        root.left?.left = TreeNode(-20)
+        root.left?.right = TreeNode(-5)
+        root.right?.left = TreeNode(-10)
+
+        assertEquals(3, maxLevelSum(root))
     }
 }
