84abfff9e2eeb8996d73c220136e74e43494fc59
arsh <arshfodcc@gmail.com>
2021-04-23 16:08:09 +0300

    Update tests

--- src/leetcode/easy/L1Test.kt
+++ src/leetcode/easy/L1Test.kt
@@ -1,21 +1,21 @@
 package leetcode.easy
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 
 class L1Test {
     @Test
     fun twoSumA() {
-        assertEquals(intArrayOf(0, 1), twoSum(intArrayOf(2, 7, 11, 15), 9))
+        assertArrayEquals(intArrayOf(0, 1), twoSum(intArrayOf(2, 7, 11, 15), 9))
     }
 
     @Test
     fun twoSumB() {
-        assertEquals(intArrayOf(1, 2), twoSum(intArrayOf(3, 2, 4), 6))
+        assertArrayEquals(intArrayOf(1, 2), twoSum(intArrayOf(3, 2, 4), 6))
     }
 
     @Test
     fun twoSumC() {
-        assertEquals(intArrayOf(0, 1), twoSum(intArrayOf(3, 3), 6))
+        assertArrayEquals(intArrayOf(0, 1), twoSum(intArrayOf(3, 3), 6))
     }
 }
