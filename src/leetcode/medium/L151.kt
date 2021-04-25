ad39fceff235bd02ea5b9fb1863a201c5e4bba58
arsh <arshfodcc@gmail.com>
2021-04-25 09:55:41 +0300

    Attemp 2

--- src/leetcode/medium/L151.kt
+++ src/leetcode/medium/L151.kt
@@ -1,5 +1,7 @@
 package leetcode.medium
 
+import java.lang.StringBuilder
+
 
 fun main() {
     val s = "  Bob    Loves  Alice   "
@@ -7,11 +9,23 @@ fun main() {
 }
 
 fun reverseWords(s: String): String {
+    val rs = s.split(" ")
+    val sb = StringBuilder()
+
+    for (i in rs.size - 1 downTo 0) {
+        if (rs[i] != "") {
+            sb.append(rs[i] + " ")
+        }
+    }
+    return sb.toString().trim()
+}
+
+fun reverseWordsA(s: String): String {
     var rs = ""
 
     if (!s.trim().contains(" "))
         return s
-
+// "Bob    Loves  Alice"
     val first = s.trim().indexOfLast { space -> space == ' ' }
     val last = s.trim().length - 1
     rs = s.trim().substring(first, last + 1).trim() + " " + rs
